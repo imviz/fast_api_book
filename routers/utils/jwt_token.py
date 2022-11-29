271ce0b6f71daf6b8364f0eb42ee160f3525cc53
vishnu edappatt <vishnu.edappatt@genproresearch.com>
2022-11-29 14:41:12 +0530

    created .env file and setup configuration

--- routers/utils/jwt_token.py
+++ routers/utils/jwt_token.py
@@ -11,16 +11,12 @@ from . import config
 
 SECRET_KEY = config.SECRET_KEY
 ALGORITHM = config.ALGORITHM
-# ACCESS_TOKEN_EXPIRE_MINUTES = config.ACCESS_TOKEN_EXPIRE_MINUTES
+ACCESS_TOKEN_EXPIRE_MINUTES = config.ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    # if expires_delta:
-    #     expire = datetime.utcnow() + expires_delta
-    # else:
-
-    expire = datetime.utcnow() + timedelta(30)
+    expire = datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
